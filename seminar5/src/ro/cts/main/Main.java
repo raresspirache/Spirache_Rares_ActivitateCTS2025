29e82df03bb366b64acd948f39a260f8900bdcd2
Spirache Rares <spirache.rares@gmail.com>
2025-03-28 14:45:53 +0200

    seminar 5 completare

--- seminar5/src/ro/cts/main/Main.java
+++ seminar5/src/ro/cts/main/Main.java
@@ -9,14 +9,22 @@ public class Main {
         IBuilder rezervareBuilder = new RezervareBuild("Ionescu Nicolae");
         Rezervare rezervare;
         Rezervare rezervare1;
+        Rezervare rezervare2;
 
         IBuilder rezervareBuilder1 = new RezervareBuild("Tache Ionescu");
 
         rezervare = rezervareBuilder.buildRezervare();
         rezervare1 = rezervareBuilder1.setGenMuzica("rock").buildRezervare();
+        rezervare1=rezervareBuilder1.setAsezareGeam(true).buildRezervare();
+        rezervare1=rezervareBuilder1.setDecorareMasa(true).buildRezervare();
+
+        IBuilder rezervareBuilder2=new RezervareBuild("Popescu Ion");
+        rezervare2=rezervareBuilder2.setGenMuzica("rap").buildRezervare();
+
 
         System.out.println(rezervare.toString());
         System.out.println(rezervare1.toString());
+        System.out.println(rezervare2.toString());
 
     }
 }
